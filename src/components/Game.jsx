d7f0bf24008cdc88c84a7cc360e64315d9d0de82
John Castberg <jpcastberg@gmail.com>
2019-07-26 01:10:45 -0700

    Add functionality to trigger game start

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -9,16 +9,30 @@ class Game extends Component {
       boardHeight: 10,
       boardWidth: 10,
       numberOfMines: 10,
+      gameStarted: false,
     };
+    this.toggleGameStarted = this.toggleGameStarted.bind(this);
+  }
+
+  toggleGameStarted() {
+    const { state: { gameStarted } } = this;
+    this.setState({ gameStarted: !gameStarted });
   }
 
   render() {
-    const { state: { boardHeight, boardWidth, numberOfMines } } = this;
+    const {
+      state: {
+        boardHeight, boardWidth, numberOfMines, gameStarted,
+      },
+      toggleGameStarted,
+    } = this;
     return (
       <Board
         boardHeight={boardHeight}
         boardWidth={boardWidth}
         numberOfMines={numberOfMines}
+        gameStarted={gameStarted}
+        toggleGameStarted={toggleGameStarted}
       />
     );
   }
